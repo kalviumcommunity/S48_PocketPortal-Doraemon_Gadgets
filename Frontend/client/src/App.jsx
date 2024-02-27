507e8dd3a131547bd99076b744d2418a655b70eb
pawanomprakash <141708273+pawanomprakash@users.noreply.github.com>
2024-02-27 12:28:11 +0530

    added Gadgetoftheday entity

--- Frontend/client/src/App.jsx
+++ Frontend/client/src/App.jsx
@@ -2,16 +2,21 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import pocket from './assets/images.png'
+import GadgetOfTheDay from './components/GadgetOfTheDay'
 function App() {
 
   return (
     <>
     <Navbar/>
-    <div id="welcomepage">
-     <h1>Welcome to pocket portal </h1>
-     <p>Pick your favourite Gadgets and add it to your pocket</p>
+    <h1 id="welcomeh1">Welcome to pocket portal </h1>
+    <p>Pick your favourite Gadgets and add it to your pocket</p>
+    <div id="welcomepage" className='flex'>
+      <GadgetOfTheDay/>
+      <div>
+         <img id="pocket" src={pocket} alt="" />
+     </div>
     </div>
-    <img id="pocket" src={pocket} alt="" />
+
     </>
   )
 }
