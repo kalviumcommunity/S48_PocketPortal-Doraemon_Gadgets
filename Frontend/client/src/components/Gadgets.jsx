bc9c61ec3665ffa1f3352688ee65b9fa9735aa58
pawanomprakash <141708273+pawanomprakash@users.noreply.github.com>
2024-03-14 10:51:01 +0530

    updated

--- Frontend/client/src/components/Gadgets.jsx
+++ Frontend/client/src/components/Gadgets.jsx
@@ -41,6 +41,8 @@ import png1 from '../assets/png1.png'
 
   const handleLogout = () => {
     document.cookie = 'username=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/';
+    document.cookie = 'acessToken=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/';
+
     navigate('/login');
     document.cookie? console.log(document.cookie):console.log("No cookies found")
   };
