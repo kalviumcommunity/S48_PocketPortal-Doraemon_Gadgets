1321006ce33c68eae5be41724247f2e6084af554
pawanomprakash <141708273+pawanomprakash@users.noreply.github.com>
2024-03-19 09:24:07 +0530

    updated port

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8000;
+const port =  process.env.PORT || 8000;
 const mongoose=require('mongoose');
 require('dotenv').config()
 const mongoURI = process.env.MONGODB_URI
